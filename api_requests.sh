45a3f59e7bc39e7c867c4b2f352a30b8ccc1b023
Ryan Earl <ryanleoearl@gmail.com>
2023-05-22 21:11:09 -0700

    Added uuid retireval to getUserLogin

--- api_requests.sh
+++ api_requests.sh
@@ -74,3 +74,17 @@ password="Testpassword123!"
 curl -X POST "$url" \
   -H "Content-Type: application/json" \
   -d "{\"username\":\"$username\",\"password\":\"$password\"}"
+
+# User Login
+apiUrl='https://7pophi9tm0.execute-api.us-west-2.amazonaws.com/main/user/login'
+username='RyanEarl2'
+password='Testpassword123!'
+# Valid Credentials
+echo "Testing with valid credentials"
+response=$(curl -s -X POST $apiUrl -d "{\"username\":\"$username\",\"password\":\"$password\"}")
+echo "Response: $response"
+
+# Invalid Credentials
+echo "Testing with invalid credentials"
+response=$(curl -s -X POST $apiUrl -d "{\"username\":\"invalid\",\"password\":\"invalid\"}")
+echo "Response: $response"
--- back-end/src/functions/getUserLoginID.js
+++ back-end/src/functions/getUserLoginID.js
@@ -1,6 +1,25 @@
 const AWS = require('aws-sdk');
 const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider();
 
+async function getSub(username, userPoolId) {
+  const params = {
+    UserPoolId: userPoolId,
+    Filter: `username = "${username}"`,
+    Limit: 1
+  };
+
+  try {
+    const response = await cognitoIdentityServiceProvider.listUsers(params).promise();
+    if (response.Users.length > 0) {
+      const user = response.Users[0];
+      const subAttribute = user.Attributes.find(attribute => attribute.Name === 'sub');
+      return subAttribute.Value;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
 exports.handler = async (event) => {
   const { username, password } = JSON.parse(event.body);
   const userPoolId = 'us-west-2_ekoh7tmz0';
@@ -16,17 +35,18 @@ exports.handler = async (event) => {
     }
   };
 
+  const response = {};
+
   try {
-    const response = await cognitoIdentityServiceProvider.adminInitiateAuth(params).promise();
-    const idToken = response.AuthenticationResult.IdToken;
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ idToken })
-    };
+    const cognitoResponse = await cognitoIdentityServiceProvider.adminInitiateAuth(params).promise();
+    const idToken = cognitoResponse.AuthenticationResult.IdToken;
+    uuid = await getSub(username, userPoolId);
+    console.log(`uuid: ${uuid}`);
+    response.statusCode = 200;
+    response.body = JSON.stringify({ idToken, uuid });
   } catch (error) {
-      return {
-        statusCode: 401,
-        body: JSON.stringify({ error: error.message })
-      };
+      response.statusCode = 401;
+      response.body = JSON.stringify({ error: error.message });
   }
+  return response;
 };
