2d916ffee67069d7a3981d8b1aa75652ac3f9c99
Ryan Earl <ryanleoearl@gmail.com>
2023-05-22 22:15:31 -0700

    Added section to user creation that also adds them to the database

--- back-end/src/functions/userCreation.js
+++ back-end/src/functions/userCreation.js
@@ -1,5 +1,50 @@
 const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient();
 const cognito = new AWS.CognitoIdentityServiceProvider();
+
+async function getSub(username, userPoolId) {
+  const params = {
+    UserPoolId: userPoolId,
+    Filter: `username = "${username}"`,
+    Limit: 1
+  };
+
+  try {
+    const response = await cognito.listUsers(params).promise();
+    if (response.Users.length > 0) {
+      const user = response.Users[0];
+      const subAttribute = user.Attributes.find(attribute => attribute.Name === 'sub');
+      return subAttribute.Value;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function addUser(tableName, userId) {
+  const params = {
+    TableName: tableName,
+    Key: { id: 'user' },
+    UpdateExpression: 'SET #entityName.#userId = :newUserDetails',
+    ExpressionAttributeNames: {
+      '#entityName': 'entityName',
+      '#userId': userId
+    },
+    ExpressionAttributeValues: {
+      ':newUserDetails': {
+        'owned-businesses': [],
+        photos: [],
+        reviews: []
+      }
+    }
+  };
+  try {
+    await docClient.update(params).promise();
+    console.log(`Added user ${userId} to entityName object`);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 exports.handler = async (event) => {
   const body = JSON.parse(event.body);
@@ -12,6 +57,14 @@ exports.handler = async (event) => {
       Username: username,
       Password: password,
     }).promise();
+
+    await cognito.adminConfirmSignUp({
+      UserPoolId: userPoolId,
+      Username: username
+    }).promise();
+
+    uuid = await getSub(username, userPoolId)
+    await addUser('api-gateway-test', uuid);
     return { success: true };
   } catch (error) {
     console.error(error);
