3e3fdfc63ad3b9cf0ff0b01933b3efff3ead8445
Ryan Earl <ryanleoearl@gmail.com>
2023-06-04 16:42:17 -0700

    Added additional dependencies and permissions for uploadImage lambda

--- back-end/src/functions/uploadImage.js
+++ back-end/src/functions/uploadImage.js
@@ -1,22 +1,29 @@
 const AWS = require('aws-sdk');
+const multipart = require('aws-lambda-multipart-parser');
 const s3 = new AWS.S3();
 
 exports.handler = async (event) => {
-  console.log(event.body);
+  const result = multipart.parse(event);
+  console.log(result);
   const bucket = 'cs-493-restful-api-main-253515352635'
-  const imageName = event.queryStringParameters.imageName;
+  const contentDispositionHeader = event.body.match(/Content-Disposition: form-data; name=""; filename="(.+)"/i);
+  console.log(`THIS TIS THE EXTRACT HEADER DATA: ${contentDispositionHeader}`);
+  const imageName = contentDispositionHeader ? contentDispositionHeader[1] : 'default.jpg';
+  const contentType = event.headers['Content-Type'] || 'image/jpeg';
+  console.log(contentType);
+  console.log(`THIS IS THE IMAGE NAME: ${imageName}`);
   const binaryImageData = Buffer.from(event.body, 'base64');
   try {
     await s3.putObject({
       Bucket: bucket,
-      Key: imageName,
+      Key: `images/${imageName}`,
       Body: binaryImageData,
-      ContentType: 'image/jpeg'
+      ContentType: contentType
     }).promise();
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: 'Image uploaded successfully' })
+      body: JSON.stringify({ message: `Image uploaded successfully: https://cs-493-restful-api-main-253515352635.s3.us-west-2.amazonaws.com/images/${imageName}` })
     };
   } catch (error) {
     console.error(error);
