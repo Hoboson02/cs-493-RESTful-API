f9506ce998521b9c0fcbe9e992d95d7fed6c7e26
Ryan Earl <ryanleoearl@gmail.com>
2023-06-04 18:30:53 -0700

    Image URL is now added to new entry in dynamodb when it is uploaded to s3

--- back-end/src/functions/uploadImage.js
+++ back-end/src/functions/uploadImage.js
@@ -1,8 +1,58 @@
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
+import {
+  DynamoDBDocumentClient,
+  UpdateCommand
+} from "@aws-sdk/lib-dynamodb";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+const client = new DynamoDBClient({region: 'us-west-2'});
+const dynamoDb = DynamoDBDocumentClient.from(client);
+
+async function updateNestedObject(tableName, primaryKey, primaryValue, nestedObjectKeys, newDatas) {
+  const newData = JSON.parse(newDatas);
+  const updateExpression = Object.keys(newData).map((key) => {
+    const sanitizedKey = key.replace(/[^a-zA-Z0-9]/g, '');
+    return `#${nestedObjectKeys.join('.#')}.#${sanitizedKey} = :${sanitizedKey}`;
+  }).join(", ");
+  const expressionAttributeNames = nestedObjectKeys.reduce((acc, key) => {
+    acc[`#${key}`] = key;
+    return acc;
+  }, {});
+  Object.keys(newData).forEach((key) => {
+    const sanitizedKey = key.replace(/[^a-zA-Z0-9]/g, '');
+    expressionAttributeNames[`#${sanitizedKey}`] = key;
+  });
+  const expressionAttributeValues = Object.entries(newData).reduce((acc, [key, value]) => {
+    const sanitizedKey = key.replace(/[^a-zA-Z0-9]/g, '');
+    acc[`:${sanitizedKey}`] = value;
+    return acc;
+  }, {});
+  const params = {
+    TableName: tableName,
+    Key: {
+      [primaryKey]: primaryValue
+    },
+    UpdateExpression: `SET ${updateExpression}`,
+    ExpressionAttributeNames: expressionAttributeNames,
+    ExpressionAttributeValues: expressionAttributeValues
+  };
+  try {
+    await dynamoDb.send(new UpdateCommand(params));
+    console.log("Update succeeded");
+  } catch (err) {
+    console.error("Update failed", err);
+  }
+}
 
 exports.handler = async (event) => {
-  console.log(event.body);
+  var path = JSON.stringify(event['path']);
+  path.replace('"','');
+  const pathArray = path.split("/");
+  pathArray.shift();
+  pathArray.shift();
+  pathArray[pathArray.length-1] = pathArray[pathArray.length-1].replace('\"','');
+  pathArray.unshift("entityName");
+  console.log(pathArray);
   const bucket = 'cs-493-restful-api-main-253515352635'
   const contentDispositionHeader = event.body.match(/Content-Disposition: form-data; name=""; filename="(.+)"/i);
   console.log(`THIS TIS THE EXTRACT HEADER DATA: ${contentDispositionHeader}`);
@@ -14,6 +64,7 @@ exports.handler = async (event) => {
   const binaryImageData = Buffer.from(event.body, 'base64');
   let date = new Date().toJSON();
   let result = date.split('.')[0];
+  let imageAddress = `https://cs-493-restful-api-main-253515352635.s3.us-west-2.amazonaws.com/images/${result}.${imageType}`;
   try {
     await s3.putObject({
       Bucket: bucket,
@@ -21,11 +72,13 @@ exports.handler = async (event) => {
       Body: binaryImageData,
       ContentType: contentType
     }).promise();
-
+    const newData = JSON.stringify({result : imageAddress});
+    console.log(newData);
+    await updateNestedObject("cs-493-restful-api-main-data", "id", "business", [ 'entityName', 'testBusiness1', 'photos' ], '{"2023-06-05T01:19:07": "https://cs-493-restful-api-main-253515352635.s3.us-west-2.amazonaws.com/images/2023-06-05T01:19:07.png"}');
     return {
       
       statusCode: 200,
-      body: JSON.stringify({ message: `Image uploaded successfully: https://cs-493-restful-api-main-253515352635.s3.us-west-2.amazonaws.com/images/${result}.${imageType}` })
+      body: JSON.stringify({ message: `Image uploaded successfully: ${imageAddress}` })
     };
   } catch (error) {
     console.error(error);
